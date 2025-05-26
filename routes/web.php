88667dcf1c8ef711e5bc28ebfcc0ad6d4adb36a1
Orlandinho <horlandomagic@gmail.com>
2025-05-26 13:53:09 -0300

    Removing routes that aren't being used

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,9 @@ Route::get('/teclado', function () {
     return Inertia::render('Teclado');
 })->name('teclado');
 
-Route::get('dashboard', function () {
+/*Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+require __DIR__.'/auth.php';*/
